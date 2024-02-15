4202253bd66bbf5f034ce6a96053e30b2372b212
ksalarang <anbleidd@gmail.com>
2024-02-15 21:16:20 +0500

    add pirate behaviour:
    Pirate attacks when clicking on him

---
+++ Assets/Scripts/Creatures/Pirate.cs
@@ -0,0 +1,26 @@
+﻿using Spine.Unity;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Creatures {
+public class Pirate : MonoBehaviour, IPointerClickHandler {
+    static readonly string Idle = "Idle";
+    static readonly string Attack1 = "Attack_1";
+    static readonly string Attack2 = "Attack_2";
+    new SkeletonAnimation animation;
+
+    void Awake() {
+        animation = GetComponent<SkeletonAnimation>();
+    }
+
+    void Start() {
+        animation.AnimationState.SetAnimation(0, Idle, true);
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        var animationName = Random.value < 0.5f ? Attack1 : Attack2;
+        animation.AnimationState.SetAnimation(0, animationName, false);
+        animation.AnimationState.AddAnimation(0, Idle, true, 0);
+    }
+}
+}
