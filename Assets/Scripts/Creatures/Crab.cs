57ab806b39624f7973cb3d3e933b66d723067f01
ksalarang <anbleidd@gmail.com>
2024-02-15 20:57:40 +0500

    add crab behaviour:
    Crabs walks back and forth on the screen.

---
+++ Assets/Scripts/Creatures/Crab.cs
@@ -0,0 +1,48 @@
+﻿using Spine.Unity;
+using UnityEngine;
+
+namespace Creatures {
+public class Crab : MonoBehaviour {
+    [SerializeField] float moveSpeed;
+
+    new Transform transform;
+    new SkeletonAnimation animation;
+    Vector3 initialPosition;
+    [SerializeField] Vector3 destination;
+    [SerializeField] float time;
+    [SerializeField] float totalTime;
+
+    void Awake() {
+        transform = base.transform;
+        animation = GetComponent<SkeletonAnimation>();
+        
+        initialPosition = transform.localPosition;
+        destination = initialPosition;
+        destination.x = -destination.x;
+
+        totalTime = Vector3.Distance(initialPosition, destination) / moveSpeed;
+    }
+
+    void Start() {
+        animation.AnimationState.SetAnimation(0, "Walk", true);
+    }
+
+    void Update() {
+        time += Time.deltaTime;
+        if (time < totalTime) {
+            transform.localPosition = Vector3.Lerp(initialPosition, destination, time / totalTime);
+        } else {
+            time = 0;
+            transform.localPosition = destination;
+            (initialPosition, destination) = (destination, initialPosition);
+            flipX();
+        }
+    }
+
+    void flipX() {
+        var angles = transform.eulerAngles;
+        angles.y = transform.localPosition.x < 0 ? 180 : 0;
+        transform.eulerAngles = angles;
+    }
+}
+}
--- Assets/Scripts/FirstScene/FirstSceneController.cs
+++ Assets/Scripts/FirstScene/FirstSceneController.cs
@@ -3,9 +3,12 @@ using UnityEngine.SceneManagement;
 
 namespace FirstScene {
 public class FirstSceneController : MonoBehaviour {
+    void Awake() {
+        Application.targetFrameRate = 60;
+    }
+
     public void loadNextScene() {
         SceneManager.LoadScene(1);
-        
     }
 }
 }
